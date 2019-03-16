342af064d81e59db31c1d7a74e7accd5cfa7f368
DongQuanRui <48600426+DongQuanRui@users.noreply.github.com>
2019-03-16 15:03:04 -0700

    Add files via upload

---
+++ models.cpp
@@ -0,0 +1,72 @@
+//
+//  pricemodel.cpp
+//  EE599project
+//
+//  Created by zhendong ju on 3/14/19.
+//  Copyright © 2019 zhendong ju. All rights reserved.
+//
+
+#include "models.h"
+#define a 0.5
+#define b 10
+
+//double pricemodel::calculate_price(double time_start, double time_end){
+//    double price[24] = {0.12, 0.156, 0.165, 0.117, 0.194, 0.192, 0.318, 0.266, 0.326, 0.293, 0.388, 0.359, 0.447, 0.478, 0.513, 0.491, 0.457, 0.506, 0.640, 0.544, 0.592, 0.486, 0.499, 0.292};
+//    double total_price = 0;
+//    int time_start_24 = int(time_start) % 24;
+//    int time_end_24 = int(time_end) % 24;
+//
+//    total_price += ((time_start_24 + 1 - time_start) / 1) * price[time_start_24];
+//    total_price += ((time_end - time_end_24) / 1) * price[time_end_24];
+//
+//    for (int i=int(time_start) + 1; i<=int(time_end); i++) {
+//        total_price += price[(i%24)];
+//    }
+//
+//    return total_price;
+//}
+
+double Models::calculate_power(double time_s, double time_e, double cpu_usage){
+    
+    double price[24] = {0.12, 0.156, 0.165, 0.117, 0.194, 0.192, 0.318, 0.266, 0.326, 0.293, 0.388, 0.359, 0.447, 0.478, 0.513, 0.491, 0.457, 0.506, 0.640, 0.544, 0.592, 0.486, 0.499, 0.292};
+    double total_price = 0;
+    int time_start_24 = int(time_start) % 24;
+    int time_end_24 = int(time_end) % 24;
+    
+    //分情况讨论算钱
+    if (cpu_usage < 0.7)
+    {
+        total_price += ((time_start_24 + 1 - time_start) / 1.0) * (cpu_usage * a) * price[time_start_24];
+        total_price += ((time_end - time_end_24) / 1.0) * (cpu_usage * a) * price[time_end_24];
+        
+        for (int i=int(time_start) + 1; i<=int(time_end); i++) {
+            total_price += (cpu_usage * a) * price[(i%24)];
+        }
+
+        return total_price;
+    }else{
+        
+        total_price += ((time_start_24 + 1 - time_start) / 1.0) * (0.7 * a + b * (cpu_usage_big - 0.7) * (cpu_usage_big - 0.7)) * price[time_start_24];
+        total_price += ((time_end - time_end_24) / 1.0) * (0.7 * a + b * (cpu_usage_big - 0.7) * (cpu_usage_big - 0.7)) * price[time_end_24];
+        
+        for (int i=int(time_start) + 1; i<=int(time_end); i++) {
+            total_price += (0.7 * a + b * (cpu_usage_big - 0.7) * (cpu_usage_big - 0.7)) * price[(i%24)];
+        }
+        
+        return total_price;
+    }
+}
+
+/*
+cpu_usage * a
+ 
+ 
+*/
+
+
+
+
+
+
+
+
---
+++ models.h
@@ -0,0 +1,30 @@
+//
+//  pricemodel.hpp
+//  EE599project
+//
+//  Created by zhendong ju on 3/14/19.
+//  Copyright © 2019 zhendong ju. All rights reserved.
+//
+
+#ifndef pricemodel_hpp
+#define pricemodel_hpp
+
+#include <iostream>
+
+using namespace std;
+
+//class pricemodel{
+//public:
+//    double calculate_price(double time_start, double time_end);
+//    //double get_price(int time); //time is the index of price array
+//};
+
+class Models{
+public:
+    double calculate_price(){
+        
+    }
+};
+
+
+#endif /* pricemodel_hpp */
